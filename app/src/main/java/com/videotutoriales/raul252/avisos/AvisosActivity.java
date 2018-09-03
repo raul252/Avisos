101bfec5bb7981dbf5b6ba6c0602d6a0575a9dba
raul252 <raul252@gmail.com>
2018-09-03 23:28:07 +0200

    Primer commit

--- app/src/main/java/com/videotutoriales/raul252/avisos/AvisosActivity.java
+++ app/src/main/java/com/videotutoriales/raul252/avisos/AvisosActivity.java
@@ -5,6 +5,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -40,12 +41,15 @@ public class AvisosActivity extends AppCompatActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (id) {
+            case R.id.action_nuevo:
+                Log.d(getLocalClassName(), "Crear nuevo aviso");
+                return true;
+            case R.id.action_salir:
+                finish();
+                return true;
+                default:
+                    return false;
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
