7102ec1bba3e3a11e0f22a39106c2057b37f01ce
raul252 <raul252@gmail.com>
2018-09-03 23:37:24 +0200

    Primer commit

---
+++ app/src/main/java/com/videotutoriales/raul252/avisos/Aviso.java
@@ -0,0 +1,39 @@
+package com.videotutoriales.raul252.avisos;
+
+public class Aviso {
+    private int mId;
+
+    public int getID() {
+        return mId;
+    }
+
+    public void setId(int mID) {
+        this.mId = mID;
+    }
+
+    public String getContent() {
+        return mContent;
+    }
+
+    public void setContent(String mContent) {
+        this.mContent = mContent;
+    }
+
+    private String mContent;
+
+    public int getImportant() {
+        return mImportant;
+    }
+
+    public void setImportant(int mImportant) {
+        this.mImportant = mImportant;
+    }
+
+    private int mImportant;
+
+    public  Aviso(int id, String content, int important){
+        mId= id;
+        mContent=content;
+        mImportant=important;
+    }
+}
